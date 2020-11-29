1ba9a68fa359963949c6cddadbf571c12ada6bc8
WSParker <will.parker0@gmail.com>
2020-11-28 20:20:16 -0800

    sitie class with show and save methods

--- wsp_tools/sitie.py
+++ wsp_tools/sitie.py
@@ -1,8 +1,136 @@
 from .beam import dB
 from .constants import *
+from .cielab import rgba
 import numpy as np
 import scipy.ndimage as ndi
 np.seterr(divide='ignore')
+from matplotlib.pyplot import subplots, imshow, quiver, show, tight_layout, savefig
+import os
+import ncempy.io.dm as dm
+import errno
+from shutil import copy
+import json
+
+class lorentz:
+	def __init__(self, fdir, fname):
+		dm3file = dm.dmReader(os.path.join(fdir, fname))
+		self.rawData = dm3file['data']
+		self.data = dm3file['data']
+		self.fname = os.path.splitext(dm3file['filename'])[0]
+		if dm3file['pixelUnit'] == 'µm':
+			self.pixelSize = dm3file['pixelSize'][0] * 1e-6
+		elif dm3file['pixelUnit'] == 'nm':
+			self.pixelSize = dm3file['pixelSize'][0] * 1e-9
+		elif dm3file['pixelUnit'] == 'mm':
+			self.pixelSize = dm3file['pixelSize'][0] * 1e-3
+		else:
+			self.pixelSize = dm3file['pixelSize'][0]
+		self.rawSITIE = SITIE(self.rawData, 1e-3, self.pixelSize)
+		self.metadata = {'pixelSize':float(self.pixelSize)}
+		self.sitie(1e-3)
+
+	def sitie(self, defocus, wavelength=1.96e-12):
+		self.metadata.update({'defocus': defocus})
+		dummy = sitie_RHS(self.data, defocus, wavelength)
+		self.phase = np.real(inverse_laplacian(dummy, self.pixelSize))
+		self.Bx, self.By = B_from_phase(self.phase, thickness=1)
+
+	def crop_pixel_counts(self, sigma=10):
+		self.metadata.update({'crop pixel sigma': sigma})
+		self.data = crop_pixel_values(self.data, sigma=sigma)
+
+	def high_pass(self, sigma=50):
+		self.metadata.update({'high pass sigma': sigma})
+		self.data = high_pass(self.data, sigma=sigma)
+
+	def low_pass(self, sigma=50):
+		self.metadata.update({'low pass sigma': sigma})
+		self.data = low_pass(self.data, sigma=sigma)
+
+	def blur(self, sigma=5, mode='wrap', cval=0.0):
+		self.metadata.update({'blur sigma': sigma})
+		self.data = blur(self.data, sigma, mode, cval)
+
+	def show(self, window=((0,-1), (0, -1)), quiver_step=32):
+		((xmin, xmax), (ymin, ymax)) = window
+		if xmax == -1:
+			xmax = self.data.shape[0]
+		if ymax == -1:
+			ymax = self.data.shape[1]
+		xrange = np.arange(xmin,xmax,quiver_step)
+		yrange = np.arange(ymin,ymax,quiver_step)
+		data = self.data[ymin:ymax, xmin:xmax]
+		phase = self.phase[ymin:ymax, xmin:xmax]
+		Bx, By = self.Bx[ymin:ymax, xmin:xmax], self.By[ymin:ymax, xmin:xmax]
+		fig, ax = subplots(nrows=3, ncols=1, sharex=True, sharey=True, figsize=(6,18))
+		(ax1, ax2, ax3) = ax
+		ax1.set_title("Intensity - {:}".format(self.fname))
+		ax1.set_ylabel("y (px)")
+		ax1.imshow(data, origin='lower')
+		ax2.set_title("Phase")
+		ax2.set_ylabel("y (px)")
+		ax2.imshow(phase, origin='lower')
+		ax3.set_title("Phase")
+		ax3.set_xlabel("x (px)")
+		ax3.set_ylabel("y (px)")
+		ax3.imshow(rgba(Bx+1j*By), origin='lower')
+		ax3.quiver(xrange, yrange, By[::quiver_step,::quiver_step], Bx[::quiver_step,::quiver_step], color='white')
+		tight_layout()
+		show()
+
+	def save(self, window=((0,-1), (0, -1)), quiver_step=32, outdir=None,
+			savedm3=False, metadata={}):
+		((xmin, xmax), (ymin, ymax)) = window
+		if xmax == -1:
+			xmax = self.data.shape[0]
+		if ymax == -1:
+			ymax = self.data.shape[1]
+		xrange = np.arange(xmin,xmax,quiver_step)
+		yrange = np.arange(ymin,ymax,quiver_step)
+		data = self.data[ymin:ymax, xmin:xmax]
+		phase = self.phase[ymin:ymax, xmin:xmax]
+		Bx, By = self.Bx[ymin:ymax, xmin:xmax], self.By[ymin:ymax, xmin:xmax]
+		if outdir is None:
+			outdir = os.getcwd()
+		outdir = os.path.join(outdir, self.fname+"_x_{:}_{:}_y_{:}_{:}".format(xmin,xmax,ymin,ymax))
+		if not os.path.exists(outdir):
+			try:
+				os.makedirs(outdir, 0o700)
+			except OSError as e:
+				if e.errno != errno.EEXIST:
+					raise
+		fig, ax = subplots(nrows=1, ncols=1, figsize=(6,6))
+		ax.set_title("Intensity - {:}".format(self.fname))
+		ax.set_xlabel("x (px)")
+		ax.set_ylabel("y (px)")
+		ax.imshow(data, origin='lower')
+		tight_layout()
+		savefig(os.path.join(outdir, "intensity.png"))
+
+		fig, ax = subplots(nrows=1, ncols=1, figsize=(6,6))
+		ax.set_title("Phase - {:}".format(self.fname))
+		ax.set_xlabel("x (px)")
+		ax.set_ylabel("y (px)")
+		ax.imshow(phase, origin='lower')
+		tight_layout()
+		savefig(os.path.join(outdir, "phase.png"))
+
+		fig, ax = subplots(nrows=1, ncols=1, figsize=(6,6))
+		ax.set_title("B Field - {:}".format(self.fname))
+		ax.set_xlabel("x (px)")
+		ax.set_ylabel("y (px)")
+		ax.imshow(rgba(Bx+1j*By), origin='lower')
+		ax.quiver(xrange, yrange, By[::quiver_step,::quiver_step], Bx[::quiver_step,::quiver_step], color='white')
+		tight_layout()
+		savefig(os.path.join(outdir, "BField.png"))
+
+		if savedm3:
+			copy(os.path.join(fdir, fname), os.path.join(outdir, fname))
+
+		self.metadata.update(metadata)
+
+		with open(os.path.join(outdir, 'metadata.json'), 'w') as fp:
+			json.dump(self.metadata, fp)
 
 ############################## Pre-processing ####################
 def blur(image, sigma=5, mode='wrap', cval=0.0):
