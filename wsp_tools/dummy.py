885bf1830e707567954015fdbe80563180be678f
WSParker <will.parker0@gmail.com>
2020-12-15 14:54:43 -0800

    asdfasdf

---
+++ wsp_tools/dummy.py
@@ -0,0 +1,9 @@
+# This is a dummy file to see if I can create another version
+# In this attempt, I will:
+# 0. change setup.py version to 1.0.4
+# 1. git add .
+# 2. git commit -m 'asdfasdf'
+# 3. git tag 1.0.4
+# 4. git push --tags
+
+# Then, I'll try an install
