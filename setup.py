9686e8c8b485b70c835438d3792c43d577bd634a
WSParker <will.parker0@gmail.com>
2020-12-15 14:40:45 -0800

    wheel required

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     long_description = open('README.md').read(),
     long_description_content_type = "text/markdown",
     python_requires='>=3.6',
-    install_requires=['numpy','matplotlib','scipy']
+    install_requires=['wheel','numpy','matplotlib','scipy']
 )
